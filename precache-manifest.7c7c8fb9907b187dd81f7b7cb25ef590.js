b85a00a6bf635f37e63bb25ab65c9252103f87ff
ShionWebGym <a18dc208@dhw.ac.jp>
2020-06-04 07:14:38 +0900

    Updates

---
+++ precache-manifest.7c7c8fb9907b187dd81f7b7cb25ef590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d9fff17340b62c8a477201f6259d30",
+    "url": "/LocationRoulette/index.html"
+  },
+  {
+    "revision": "1916d47ecfbd8fba61ce",
+    "url": "/LocationRoulette/static/css/main.e0c48662.chunk.css"
+  },
+  {
+    "revision": "58c6c9cff291e61313b4",
+    "url": "/LocationRoulette/static/js/2.b55f2e21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LocationRoulette/static/js/2.b55f2e21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1916d47ecfbd8fba61ce",
+    "url": "/LocationRoulette/static/js/main.afc410d1.chunk.js"
+  },
+  {
+    "revision": "211d5603aaf6aa09347c",
+    "url": "/LocationRoulette/static/js/runtime-main.a96e218d.js"
+  }
+]);
