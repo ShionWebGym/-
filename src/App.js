0d227ba1117551418de6a0d516e3e6c5e5dbd308
ShionWebGym <a18dc208@dhw.ac.jp>
2020-06-04 07:10:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Roulette from './Roulette';
+
+function App() {
+  return (
+    <div className="App">
+     <header className="header">
+       <h1 className="h1">ロケーションルーレット！</h1>
+       <p className="p">風景SSを撮りたいけどロケーションが決まらない<br/>そんなあなたにピッタリのサイトです！</p>
+     </header>
+     <main className="main">
+     <Roulette/>
+     </main>
+     <footer className="footer"><p><small>Copyright (C) SQUARE ENIX CO., LTD. All Rights Reserved.</small></p></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Roulette.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import './index.css';
+
+const loca = [
+"外地ラノシア",
+"高地ラノシア",
+"西ラノシア",
+"東ラノシア",
+"中央ラノシア",
+"低地ラノシア",
+"ウルヴズジェイル",
+"リムサロミンサ",
+"北ザナラーン",
+"西ザナラーン",
+"東ザナラーン",
+"中央ザナラーン",
+"南ザナラーン",
+"ゴールドソーサー",
+"ウルダハ",
+"北部森林",
+"東部森林",
+"中央森林",
+"南部森林",
+"グリダニア",
+"モードゥナ",
+"クルザス西部高地",
+"クルザス中央高地",
+"イシュガルド",
+"イディルシャイア",
+"高地ドラヴァニア",
+"低地ドラヴァニア",
+"ドラヴァニア雲海",
+"アバラシア雲海",
+"アジス・ラー",
+"ギラバニア辺境地帯",
+"ギラバニア山岳地帯",
+"ギラバニア湖畔地帯",
+"ラールガーズリーチ",
+"紅玉海",
+"ヤンサ",
+"アジムステップ",
+"ドマ町人地",
+"クガネ",
+"レイクランド",
+"コルシア島",
+"アム・アレーン",
+"イル・メグ",
+"ラケティカ大森林",
+"テンペスト",
+"ユールモア",
+"クリスタリウム",
+]
+
+function roulette(){
+    const locaNo = Math.floor( Math.random() * loca.length);
+    let result = document.getElementById("result");
+    result.innerText = loca[locaNo];
+    //console.log(loca[locaNo]);
+}
+
+
+class Roulette extends Component{
+      render() {
+        return (
+          <div>
+          <p className="text1">今日のSSは</p>
+          <p id="result"></p>
+          <button id="btn" onClick={roulette}>場所を決める！</button>
+          </div>
+        );
+      }
+}
+
+export default Roulette;
